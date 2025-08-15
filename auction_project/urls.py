8505605ec77089b89c8aa328d000333111ba97e1
Kunal6156 <128970613+Kunal6156@users.noreply.github.com>
2025-08-15 21:43:29 +0530

    Update urls.py

--- auction_project/urls.py
+++ auction_project/urls.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include,re_path
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from auction_app.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('auction_app.urls')),
-    path('', index, name='index'),
-]
-urlpatterns += [
-    re_path(r'^(?!api/).*$', TemplateView.as_view(template_name="index.html")),
+    path('api/', include('auction_app.urls')),  # all backend API endpoints
+    re_path(r'^(?!api/).*$', TemplateView.as_view(template_name="index.html")),  # React SPA fallback
 ]

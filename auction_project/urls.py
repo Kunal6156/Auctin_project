09647415e00097e38f3e64a101a900793b24fc26
Kunal6156 <128970613+Kunal6156@users.noreply.github.com>
2025-08-15 21:26:45 +0530

    Update urls.py

--- auction_project/urls.py
+++ auction_project/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
 from django.views.generic import TemplateView
 from auction_app.views import index
 
@@ -7,4 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('auction_app.urls')),
     path('', index, name='index'),
+]
+urlpatterns += [
+    re_path(r'^(?!api/).*$', TemplateView.as_view(template_name="index.html")),
 ]
